15e475d4518fade61c286551582b191df6e34579
KatrinaaDing <31226246+KatrinaaDing@users.noreply.github.com>
2019-03-07 19:21:25 +1100

    Update PingClient.py

--- PingClient.py
+++ PingClient.py
@@ -1,11 +1,12 @@
 import time
 import socket
-from File import test_file
+
+time_rec = []
 
 for pings in range(10):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.settimeout(1.0)
-    message = test_file
+    message = "TEST MSG"
     addr = ("127.0.0.1", 12000)
 
     start = time.time()
@@ -15,6 +16,10 @@ for pings in range(10):
         data, server = client_socket.recvfrom(1024)
         end = time.time()
         elapsed = end - start
+        time_rec.append(elapsed)
         print("ping to " + addr[0] + ', ' + "seq = " + str(pings) + ", " + "rtt = " + str(elapsed) + "ms")
+
     except socket.timeout:
-        print('REQUEST TIMED OUT')
+        print("ping to " + addr[0] + ', ' + "seq = " + str(pings) + ", " + "rtt = " + 'PACKET LOST')
+
+print("\nmax time = {} \nmin time = {} \navg time = {}\n".format(max(time_rec), min(time_rec), sum(time_rec)/len(time_rec)))
