1c685ec0b3040a74672325d8888b66fd362cb1e9
KatrinaaDing <xiaoding1998@hotmail.com>
2019-03-15 15:15:01 +1100

    df

--- WebServer.py
+++ WebServer.py
@@ -1,55 +1,7 @@
 
-
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-
-port = 8080
-addr = ('localhost', port)
-
-# Handler = http.server.SimpleHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    
-    # Parse and get the request to determine the specific file
-    def do_GET(self):
-    	print (self.path)
-
-        try:
-        	# f = open(self.path[1:])
-        	self.rfile(self.path[1:])
-        	self.send_response(200)
-        except:
-        	file_to_open = "File Not Found"
-        	self.send_error(404)
-        
-#    def send_error(error_message_format, message=None):
-#    	print("dfdfd")
-
-
-# receive HTTP request from connection.
-
-server = HTTPServer(addr,handler)
-
-print('server started at ', port)
-
-server.serve_forever()
-
-
-'''
-
-import http.server
-import socketserver
-# 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
-'''
-'''
 from socket import *
 
-port = 80 # default web port
+port = 8080 # default web port
 soc = socket(AF_INET, SOCK_STREAM)
 
 soc.bind(('', port))
@@ -58,16 +10,14 @@ soc.listen(1)
 while 1:
 	clientSoc, addr = soc.accept()
 	msg = clientSoc.recv(1024)
+	#msg.decode()
 	file = msg.split()[1]
-	f = open(file[1:])
+	f = file[1:]
+	fp = open(f)
+	content = fp.read()
+	content = content.encode()
+	# print(content)
+	fp.close()
+	clientSoc.send(content)
 
-	clientSoc.send(f.read())
-	clientSoc.close()
-'''
-
-
-'''
-    	# self.send_response(200)
-        if self.path == '/':
-            self.path = ('/index.html')
-'''
+clientSoc.close()
