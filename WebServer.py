6f7f38673a7c472723ece83f4f88b5345f9c35cc
KatrinaaDing <xiaoding1998@hotmail.com>
2019-03-17 21:06:19 +1100

    Update WebServer.py

--- WebServer.py
+++ WebServer.py
@@ -12,10 +12,11 @@ soc.bind(('', port))
 soc.listen(1)
 
 while 1:
+	print("\nListening...")
 	clientSoc, addr = soc.accept()
 	msg = clientSoc.recv(1024)
 	
-	print("\nmsg:\n" + str(msg) + "\n\n-------------------")
+	# print("\nmsg:\n" + str(msg) + "\n\n-------------------")
 
 	# resend if header lost
 	if msg == b'': continue
@@ -23,32 +24,36 @@ while 1:
 	# extract the file name
 	file = msg.split()[1]
 	f = file[1:]
-	print("\nf:\n" + str(f) + "\n\n-------------------")
-	
+	print(f"Trying to open file {f}")
+	# print("\nf:\n" + f + "\n\n-------------------")
+
+	# only identifying .html and .png
+	if b'html' not in f and b'png' not in f: continue 
+
 	try:
 		fp = open(f, 'rb')
 
 	# if open() failed (file not exist), 404 is sent
 	except:
-		fail_msg = """
+		fail_msg = b"""
 		HTTP/1.1 404 Not Found
 
 
-		File Not Found
+		404 File Not Found
 		"""
-		print("\nfail_msg:\n" + str(fail_msg) + "\n\n-------------------")
-		fail_msg = fail_msg.encode()
+		# print("\nfail_msg:\n" + str(fail_msg) + "\n\n-------------------")
 		clientSoc.send(fail_msg)
+		print("Sorry, file not found")
 		clientSoc.close()
-
-		finish = input("finish? (y/n)")
-		if finish == 'y': break
-
+		'''
+		finish = input("\nWish to continue? (y/n)")
+		if finish == 'n': break
+		'''
 		continue
 	
 	# if successfully opened the file, send 200 and identify file type
 	if  b'html' in f:
-		ok_msg = """
+		ok_msg = b"""
 		HTTP/1.1 200 OK 
 		Content-Type: text/html
 
@@ -56,25 +61,26 @@ while 1:
 		"""
 
 	elif b'png' in f:
-		ok_msg = """
+		ok_msg = b"""
 		HTTP/1.1 200 OK
 		Content-Type: image/png
 
 
 		"""
-	print("\nok_msg:\n" + ok_msg + "\n\n-------------------")
-	ok_msg = ok_msg.encode()
-
+	
+	# print("\nok_msg:\n" + ok_msg + "\n\n-------------------")
 	content = fp.read()
-	print("\ncontent:\n" + str(content) + "\n\n-------------------")
+	print("Reading file...")
+	#print("\ncontent:\n" + str(content) + "\n\n-------------------")
 	fp.close()
-	print("========file end==sending data========\n")
+	print("Sending data...")
 
 	# ok_msg += content
 	clientSoc.send(ok_msg)
 	clientSoc.send(content)
-	# print(ok_msg)
+	print("File is successfully sent\n")
 	clientSoc.close()
-
-	finish = input("finish? (y/n)")
-	if finish == 'y': break
+	'''
+	finish = input("\nWish to continue? (y/n)")
+	if finish == 'n': break
+	'''
